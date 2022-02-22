d12278003cb6275c31c98134a2129dbfd743b814
Ryan D. Friese <ryan.friese@pnnl.gov>
2022-02-22 11:50:18 -0800

    build rofi if  ROFI_DIR not provided

--- build.rs
+++ build.rs
@@ -1,45 +1,75 @@
 extern crate bindgen;
 extern crate subprocess;
 
+use autotools;
 use std::env;
 use std::path::PathBuf;
+use glob::glob;
 
 fn build_bindings() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    let ofi_env = env::var("OFI_DIR").expect("The OFI_DIR environment variable must be specified with the location of the OFI installation");
-    assert!(ofi_env.len() > 0,"The OFI_DIR environment variable must be specified with the location of the OFI installation");
+    let ofi_env = match env::var("OFI_DIR"){
+        Ok(val) => {
+            println!("cargo:rustc-link-lib=dylib=fabric");
+            std::path::PathBuf::from(val)
+        },
+        Err(_) => {
+            panic!("Error: OFI_DIR not set. Please specify the root directory of your libfabrics installation.");
+        }
+    };
+    let ofi_lib_dir = ofi_env.join("lib");
+    let ofi_inc_dir = ofi_env.join("include");
+    
+    let rofi_env = match env::var("ROFI_DIR"){
+        Ok(val) => {
+            println!("cargo:rustc-link-lib=static=rofi");
+            println!("cargo:rustc-link-lib=static=pmi_simple");
+            std::path::PathBuf::from(val)
+        },
+        Err(_) => {
+            println!("cargo:rustc-link-lib=static=pmi_simple");
+            println!("cargo:rustc-link-lib=static=rofi");
+           
+            autotools::Config::new("rofi")
+            .reconf("--install")
+            .ldflag(format!{"-L{}",ofi_lib_dir.display()})
+            .cflag(format!{"-I{}",ofi_inc_dir.display()})
+            .build()
+        }
+        
+    };
+    for entry in glob("rofi/**/*.c").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => println!("cargo:rerun-if-changed={}", path.display()),
+            Err(_) => {},
+        }
+    }
+    for entry in glob("rofi/**/*.h").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => println!("cargo:rerun-if-changed={}", path.display()),
+            Err(_) => {},
+        }
+    }
+    
 
-    let rofi_env = env::var("ROFI_DIR").expect("The ROFI_DIR environment variable must be specified with the location of the Rust-OFI installation");
-    assert!(rofi_env.len() > 0,"The ROFI_DIR environment variable must be specified with the location of the OFI installation");
+    let rofi_inc_dir = rofi_env.join("include");
+    let rofi_lib_dir = rofi_env.join("lib");
 
-    println!("cargo:rustc-link-lib=dylib=fabric");
-
-    let ca_1 = "-I".to_string() + ofi_env.as_str() + "/include";
-    let ca_2 = ca_1.to_string() + "/rdma";
-
-    let ofi_lib_dir = ofi_env.as_str().to_owned() + "/lib";
-
-    let rofi_inc_dir = "-I".to_string() + rofi_env.as_str() + "/include";
-    let rofi_lib_dir = rofi_env + "/lib";
-    println!("cargo:rustc-link-search=native={}", rofi_lib_dir);
-    println!("cargo:rustc-link-search=native={}", ofi_lib_dir);
+    println!("cargo:rustc-link-search=native={}", rofi_lib_dir.display());
+    println!("cargo:rustc-link-search=native={}", ofi_lib_dir.display());
 
     println!("cargo:rustc-link-search=native=/usr/lib64");
     println!("cargo:rustc-link-search=native=/usr/lib64/libibverbs");
-
-
-    println!("cargo:rustc-link-lib=dylib=fabric");
     println!("cargo:rustc-link-lib=dylib=ibverbs");
-
-    println!("cargo:rustc-link-lib=dylib=rofi");
+    
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_arg("-I/usr/include")
-        .clang_arg(ca_1)
-        .clang_arg(ca_2)
-        .clang_arg(rofi_inc_dir)
+        .clang_arg(format!{"-I{}",ofi_inc_dir.display()})
+        .clang_arg(format!{"-I{}/rdma",ofi_inc_dir.display()})
+        .clang_arg(format!("-I{}",rofi_inc_dir.display()))
         .generate()
         .expect("Unable to generate bindings");
 
